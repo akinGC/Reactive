1ad06b416f76af0a49cb6b8743fbc6854fb92d42
akinGC <akarshgc1998@gmail.com>
2022-08-31 17:17:20 +0530

    LocationOfExpenditure

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Expense Items</h1>
+
+<h4>Food Rs 10 - <span className="LocationOfExpenditure ">New delhi</span></h4>
+
+<h4>Petrol Rs 100 - <span className="LocationOfExpenditure ">Moscow</span></h4>
+
+<h4>Movies Rs 200 - <span className="LocationOfExpenditure ">LA</span></h4>
     </div>
   );
 }
