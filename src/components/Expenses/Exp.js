2f66d4a2dc39c0d8e226c50fff9459e3f11a9bd1
akinGC <akarshgc1998@gmail.com>
2022-09-02 17:35:33 +0530

    delete dom

--- src/components/Expenses/Exp.js
+++ src/components/Expenses/Exp.js
@@ -1,6 +1,9 @@
 import ExpDate from "./ExpDate"
 const Exp=(props)=> {
-
+const zig=(e)=>{
+ 
+  e.target.parentElement.remove()
+}
     return(
        
         <div className="whole">
@@ -10,6 +13,7 @@ const Exp=(props)=> {
           <h5>{user.item}&nbsp;<span>{user.place}</span>
     <span><ExpDate date={user.date}></ExpDate></span>
           </h5>
+          <button onClick={zig}>click</button>
         </div>
       ))}
         </div>
--- src/components/Expenses/ExpDate.js
+++ src/components/Expenses/ExpDate.js
@@ -1,6 +1,6 @@
 
 const ExpDate=(props)=>{
-    console.log(props)
+    
     return(
         <div>
             <h5>{props.date.toLocaleString('en-US',{month:'long'})}</h5>
