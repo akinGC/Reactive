65d6cb01a2104283c349d24b5c9639031200aecd
akinGC <akarshgc1998@gmail.com>
2022-09-03 10:09:38 +0530

    fromState

--- src/components/Expenses/Form.js
+++ src/components/Expenses/Form.js
@@ -1,15 +1,33 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 
 const Form=()=>{
+    const [state,setState] = useState({
+        title:'',
+        amt:'',
+        date:new Date()
+    })
 function chngd(e){
-console.log(e.target.value)
+
+switch (e.target.name) {
+    case 'title':
+        setState({ ...state, title:e.target.value })
+        break;
+    case 'amount':
+            setState({ ...state, amt:e.target.value })
+            break;
+    case 'date':
+                setState({ ...state, date:e.target.value })
+                break;
+    default:
+        break;
+}
 }
     return(
         <Fragment>
 <form className="formg">
-<input type="text"placeholder="EXP title"onChange={chngd}></input>
-<input type="text"placeholder="exp amt" onChange={chngd}></input>
-<input type="date" onChange={chngd}></input>
+<input type="text"placeholder="EXP title"name="title" onChange={chngd}></input>
+<input type="text"placeholder="exp amt"name="amount" onChange={chngd}></input>
+<input type="date" onChange={chngd} name="date"></input>
 <button>Add</button>
 </form>
         </Fragment>
